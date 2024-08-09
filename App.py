3e4d851f66595f9b6e64978f524f37d55b55289b
MrNarman <godricknarman21@gmail.com>
2024-08-09 21:32:53 +0300

    Initial commit: Added a function to display the board and another one to update the board with regards to user input.

---
+++ App.py
@@ -0,0 +1,24 @@
+board = [' ' for _ in range(9)]
+
+def display_board(board):
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print("-" * 5)
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print("-" * 5)
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+
+def enter_move(board):
+    # The function accepts the board's current status, asks the user about their move, 
+    # checks the input, and updates the board according to the user's decision.
+
+    user_input = int(input("Enter your move. Cell 1-9: "))
+    user_input -= 1
+
+    board[user_input] = "X"
+
+    display_board(board)
+
+enter_move(board)
