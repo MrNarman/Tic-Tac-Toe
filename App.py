0019da5798858e589eaf42f150bc9fae0eef9c7a
MrNarman <godricknarman21@gmail.com>
2024-08-11 20:10:01 +0300

    Co-authored-by: ninosb2206@gmail.com <ninosb2206@gmail.com>

--- App.py
+++ App.py
@@ -1,3 +1,5 @@
+from random import randint
+
 board = [' ' for _ in range(9)]
 
 def display_board(board):
@@ -24,3 +26,35 @@ def enter_move(board):
 def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares;
     return [(index // 3, index % 3) for index, value in enumerate (board) if value == '']
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+
+    winning_combo = [
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 4, 8),
+        (2, 4, 6),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8)
+    ]
+
+    for combo in winning_combo:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] == sign:
+            return True
+    return False
+
+def draw_move(board):
+    # The function draws the computer's move and updates the board.
+    
+    free_fields = make_list_of_free_fields(board)
+    count = len(free_fields)
+    if count > 0:
+        move = randint(count)
+        row, col = free_fields[move]
+        board[row][col] = 'O'
+
+# Implement the logic  for the program
